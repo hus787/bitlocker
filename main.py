53cd02150fa0f88900de52634f735f93957f2a9f
Hussain Parsaiyan <hus787@gmail.com>
2025-03-08 20:51:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,7 @@ def generate_passwords():
         password = f"{first}{second}{digit}"
         yield password
 
-# Example usage to print generated passwords
-if __name__ == "__main__":
-    for pwd in generate_passwords():
-        print(pwd)
-        command_to_run = "manage-bde.exe -unlock d: -pw" #or anything else
-        run_command_and_interact(command_to_run, pwd)
+
 
 def run_command_and_interact(command, prompt_responses):
     """
@@ -48,6 +43,12 @@ def run_command_and_interact(command, prompt_responses):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+# Example usage to print generated passwords
+if __name__ == "__main__":
+    for pwd in generate_passwords():
+        print(pwd)
+        command_to_run = "manage-bde.exe -unlock d: -pw" #or anything else
+        run_command_and_interact(command_to_run, pwd)
 
 #Example for ping
 #run_command_and_interact("ping 127.0.0.1 -t", ["CTRL+C"]) # example using CTRL+C. Note that CTRL+C is not a regular string, and requires special handling.
