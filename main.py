ec85a4c5745ed29f75f2c3db76823b2a04fb17db
Hussain Parsaiyan <hus787@gmail.com>
2025-03-13 21:38:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -61,9 +61,9 @@ def run_command_and_interact(command, response):
 
 # Example usage to print generated passwords
 if __name__ == "__main__":
-    for pwd in generate_passwords3():
+    for pwd in generate_passwords2():
         print(pwd)
-        command_to_run = "manage-bde.exe -unlock f: -pw" #or anything else
+        command_to_run = "manage-bde.exe -unlock d: -pw" #or anything else
         run_command_and_interact(command_to_run, pwd)
 
 #Example for ping
